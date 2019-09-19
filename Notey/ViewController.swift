5401b5ec8e218abb04aec50c9fbeec1d4903c494
Dionte <Dionte_sb@yahoo.com>
2019-09-19 12:20:06 -0700

    Initial Commit

---
+++ Notey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notey
+//
+//  Created by Dionte Silmon on 9/19/19.
+//  Copyright © 2019 Dionte Silmon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
